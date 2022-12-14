dec2535d8f5140e25d7935c0d62853c30c513424
vvlee <vvlee@gmail.com>
2022-12-14 22:46:55 +0500

    add Example 006 Condition If Else

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Так записываются комментарии C#
+Console.Write("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Ну привет, ");
+    Console.WriteLine(username);
+}
