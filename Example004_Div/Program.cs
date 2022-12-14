30434dbb2a936a0a41d5e7f14ded909d0c0724d1
vvlee <vvlee@gmail.com>
2022-12-14 21:11:49 +0500

    change Program.cs

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Так записываются комментарии
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
