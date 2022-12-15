ff8db3871e114906176881b25440383a30000df9
vvlee <vvlee@gmail.com>
2022-12-15 22:16:39 +0500

    add Example 008 Magic

--- Example008_Magic/Program.cs
+++ Example008_Magic/Program.cs
@@ -3,9 +3,9 @@ Console.Clear();
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 40, ya = 1;
-int xb = 1, yb = 20; 
-int xc = 20, yc = 20;
+int xa = 30, ya = 1;
+int xb = 1, yb = 15; 
+int xc = 60, yc = 15;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -13,5 +13,34 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xb, yb);
 Console.WriteLine("+");
 
-//Console.SetCursorPosition(xc, yc);
-//Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+
+while(count < 2000)
+{
+   int what = new Random().Next(0,3); // [0; 3) 0 1 2 
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;//count = count + 1; //count += 1;
+}
