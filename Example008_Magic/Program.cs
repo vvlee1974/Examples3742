9d9b4ac1d4ef81aae6e1f2773eb197e84cfbbea9
vvlee <vvlee@gmail.com>
2022-12-15 21:44:35 +0500

    add Example 008 Magic

---
+++ Example008_Magic/Program.cs
@@ -0,0 +1,17 @@
+﻿// Так записываются комментарии C#
+Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 40, ya = 1;
+int xb = 1, yb = 20; 
+int xc = 20, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+//Console.SetCursorPosition(xc, yc);
+//Console.WriteLine("+");
