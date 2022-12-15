802b62c723a33a5a99f76d031862746c97731406
vvlee <vvlee@gmail.com>
2022-12-15 20:26:58 +0500

    fix trouble username!.ToLower()

--- Example006_ConditionIfElse/Program.cs
+++ Example006_ConditionIfElse/Program.cs
@@ -2,7 +2,7 @@
 Console.Write("Введите имя пользователя: ");
 string? username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if(username!.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША!");
 }
