7c1c9984d00287e5ca6ef018bd21f87f23741c4f
vvlee <vvlee@gmail.com>
2022-12-18 00:06:07 +0500

    Example 011 MethodArray

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // Прерывает поиск при нахождении первого индекса совпадающим с find=18
+    }
+        //index = index + 1;
+    index++;
+
+}
